010cce0dadad117d669c91f34e8b33d3f3cad0ac
kkyei <kkyei@adaptavist.com>
2022-03-15 10:17:49 +0000

    add app specific tests

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -7,38 +7,93 @@ from selenium_ui.conftest import print_timing
 from selenium_ui.confluence.pages.pages import Login, AllUpdates
 from util.conf import CONFLUENCE_SETTINGS
 
-
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
     if datasets['custom_pages']:
-        app_specific_page_id = datasets['custom_page_id']
+        app_specific_page_id = datasets['393565'] #pageid of rate page 393569
+        rate_page_id = datasets['393569']
+        rate_search_page_id = datasets['393567'] #pageid of rate search
+        rate_blog_id = datasets['393575'] #blogid of rate macro
+        rate_search_blog_id = datasets['393579'] #blogid of rate search macro
 
     # To run action as specific user uncomment code bellow.
     # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
     # just before test_2_selenium_z_log_out
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.wait_for_page_loaded()
-    #         login_page.set_credentials(username=username, password=password)
-    #         login_page.click_login_button()
-    #         if login_page.is_first_login():
-    #             login_page.first_user_setup()
-    #         all_updates_page = AllUpdates(webdriver)
-    #         all_updates_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    #  @print_timing("selenium_app_specific_user_login")
+    #  def measure():
+    #      def app_specific_user_login(username='admin', password='admin'):
+    #          login_page = Login(webdriver)
+    #          login_page.delete_all_cookies()
+    #          login_page.go_to()
+    #          login_page.wait_for_page_loaded()
+    #          login_page.set_credentials(username, password)
+    #          login_page.click_login_button()
+    #          if login_page.is_first_login():
+    #              login_page.first_user_setup()
+    #          all_updates_page = AllUpdates(webdriver)
+    #          all_updates_page.wait_for_page_loaded()
+    #      app_specific_user_login(username='admin', password='admin')
+    #  measure()
+
+    #Log to page with rate macro, wait till visible then log out
+    #log into page with rate search macro wait till visible then log out
+    # Same two with blog pages
+
 
     @print_timing("selenium_app_custom_action")
     def measure():
 
         @print_timing("selenium_app_custom_action:view_page")
         def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={393565}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.XPATH, "(//table)[1]"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
     measure()
+
+    @print_timing("selenium_app_rate_page")
+    def measure():
+
+        @print_timing("selenium_app_custom_action:rate_page")
+        def sub_measure():
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={393569}")
+            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
+            page.wait_until_visible((By.XPATH, "(//table)[1]"))  # Wait for you app-specific UI element by ID selector
+            rate = page.wait_until_visible((By.XPATH, "//a[@title='4 Star']"))
+            rate.click()
+        sub_measure()
+    measure()
+
+    @print_timing("selenium_app_rate_search")
+    def measure():
+
+        @print_timing("selenium_app_custom_action:view_rated_search_page")
+        def sub_measure():
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={393567}")
+            page.wait_until_visible((By.ID, "title-text"))
+            page.wait_until_visible((By.XPATH, "//div[@data-macro-name='rate-search']"))
+        sub_measure()
+    measure()
+
+    @print_timing("selenium_app_blog_rate")
+    def measure():
+
+        @print_timing("selenium_app_custom_action:rate_blog_page")
+        def sub_measure():
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={393575}")
+            page.wait_until_visible((By.ID, "title-text"))
+            page.wait_until_visible((By.CLASS_NAME, "rater"))
+        sub_measure()
+    measure()
+
+    @print_timing("selenium_app_blog_rate_search")
+    def measure():
+
+        @print_timing("selenium_app_custom_action:view_rated_search_blogpage")
+        def sub_measure():
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={393579}")
+            page.wait_until_visible((By.ID, "title-text"))
+            page.wait_until_visible((By.XPATH, "//div[@data-macro-name='rate-search']"))
+        sub_measure()
+    measure()
+
